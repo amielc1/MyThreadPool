8fffe01498859d73ba0239eee0117dda83423a8b
ADMIN <ADMIN@DESKTOP-TLVS4J6>
2025-05-21 20:39:46 +0300

    Add project files.

---
+++ MyThreadPool/Controllers/MyThreadPoolController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyThreadPool.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyThreadPoolController : ControllerBase
+    {
+          
+        private readonly ILogger<MyThreadPoolController> _logger;
+
+        public MyThreadPoolController(ILogger<MyThreadPoolController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetThread")]
+        public string Get()
+        { 
+            return "MyThreadPoolController -> GetThread";
+        }
+    }
+}
