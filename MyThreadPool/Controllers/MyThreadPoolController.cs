4f637228aef6bb335b1a445d3b2816d5c08964dd
Amiel Cohen <amielc1@gmail.com>
2025-05-21 20:45:12 +0300

    add

--- MyThreadPool/Controllers/MyThreadPoolController.cs
+++ MyThreadPool/Controllers/MyThreadPoolController.cs
@@ -16,7 +16,7 @@ namespace MyThreadPool.Controllers
 
         [HttpGet(Name = "GetThread")]
         public string Get()
-        { 
+        {
             return "MyThreadPoolController -> GetThread";
         }
     }
